f8d1616661e8d0abbc06c94478b1c5e7834863f4
Manuel Ferreira <manueljohnsonferreira2020@gmail.com>
2023-10-16 23:55:25 +0530

    repost

--- unipay/lib/login_page.dart
+++ unipay/lib/login_page.dart
@@ -7,26 +7,61 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
+      backgroundColor: Colors.white, // Non-nullable assertion operator used here
+
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Image.network(
+              'https://cdn.dribbble.com/users/2213143/screenshots/7971141/media/184cdea9758c029d9feef05432222403.gif', // Replace this with the URL of your GIF
+              height: 200,
+              width: 200,
+            ),
+            SizedBox(height: 20),
             TextFormField(
+              style: TextStyle(color: Colors.black),
               decoration: InputDecoration(
                 labelText: 'Username',
-                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey[800]!, fontWeight: FontWeight.bold),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.deepOrange, width: 2.0),
+                ),
+                filled: true,
+                fillColor: Colors.white,
               ),
             ),
             SizedBox(height: 20),
             TextFormField(
+              style: TextStyle(color: Colors.black),
               decoration: InputDecoration(
                 labelText: 'Password',
-                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey[800]!, fontWeight: FontWeight.bold),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.deepOrange, width: 2.0),
+                ),
+                filled: true,
+                fillColor: Colors.white,
               ),
               obscureText: true,
             ),
@@ -35,7 +70,17 @@ class LoginPage extends StatelessWidget {
               onPressed: () {
                 // TODO: Implement login functionality
               },
-              child: Text('Login'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepOrange, // Setting the button color to deep orange
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: Text(
+                'Login',
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+              ),
             ),
             SizedBox(height: 20),
             TextButton(
@@ -45,7 +90,10 @@ class LoginPage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => RegistrationPage()),
                 );
               },
-              child: Text('Create an Account'),
+              child: Text(
+                'Create an Account',
+                style: TextStyle(color: Colors.deepOrange, fontSize: 16.0),
+              ),
             ),
           ],
         ),
--- unipay/lib/registration_page.dart
+++ unipay/lib/registration_page.dart
@@ -7,33 +7,83 @@ class RegistrationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Registration'),
-      ),
+      backgroundColor: Colors.white, // Set the background color to white
+
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Image.network(
+              'https://cdn.dribbble.com/users/2213143/screenshots/7971141/media/184cdea9758c029d9feef05432222403.gif', // Replace this with the URL of your GIF
+              height: 200,
+              width: 200,
+            ),
+            SizedBox(height: 20),
             TextFormField(
+              style: TextStyle(color: Colors.black),
               decoration: InputDecoration(
                 labelText: 'Username',
-                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey[800]!, fontWeight: FontWeight.bold),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.deepOrange, width: 2.0),
+                ),
+                filled: true,
+                fillColor: Colors.white,
               ),
             ),
             SizedBox(height: 20),
             TextFormField(
+              style: TextStyle(color: Colors.black),
               decoration: InputDecoration(
                 labelText: 'Email',
-                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey[800]!, fontWeight: FontWeight.bold),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.deepOrange, width: 2.0),
+                ),
+                filled: true,
+                fillColor: Colors.white,
               ),
             ),
             SizedBox(height: 20),
             TextFormField(
+              style: TextStyle(color: Colors.black),
               decoration: InputDecoration(
                 labelText: 'Password',
-                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey[800]!, fontWeight: FontWeight.bold),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.grey[400]!),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.deepOrange, width: 2.0),
+                ),
+                filled: true,
+                fillColor: Colors.white,
               ),
               obscureText: true,
             ),
@@ -42,14 +92,27 @@ class RegistrationPage extends StatelessWidget {
               onPressed: () {
                 // TODO: Implement registration functionality
               },
-              child: Text('Register'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepOrange, // Setting the button color to deep orange
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: Text(
+                'Register',
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+              ),
             ),
             SizedBox(height: 20),
             TextButton(
               onPressed: () {
                 Navigator.pop(context); // Navigate back to the previous screen (in this case, the login page)
               },
-              child: Text('Back to Login'),
+              child: Text(
+                'Back to Login',
+                style: TextStyle(color: Colors.deepOrange, fontSize: 16.0),
+              ),
             ),
           ],
         ),
