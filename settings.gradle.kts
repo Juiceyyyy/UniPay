83bc371d9046416022bd45e3eec1bba93df8380b
Juiceyyyy <joshuamenezes65@gmail.com>
2023-10-19 15:43:18 +0530

    Android Fix

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "unipay1"
+include(":app")
