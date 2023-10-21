6ee586c7086b99a89381898aceb73de283dff0f9
Juiceyyyy <joshuamenezes65@gmail.com>
2023-10-21 22:16:41 +0530

    Fixed Google Sign In

--- lib/Screens/Signup/components/socal_sign_up.dart
+++ lib/Screens/Signup/components/socal_sign_up.dart
@@ -2,37 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:unipay/Screens/Home/home_page.dart';
 import '../../../screens/Signup/components/or_divider.dart';
 import '../../../screens/Signup/components/social_icon.dart';
 
 class SocalSignUp extends StatelessWidget {
-   const SocalSignUp({
+  const SocalSignUp({
     Key? key,
   }) : super(key: key);
 
   void updateGoogleSignInClientId() {
-    const String clientId = '952786014191-4babja8nujkbdub2rgj9ej8fq10vboss.apps.googleusercontent.com';
-
-  }
-
-  Future<void> _handleGoogleSignIn() async {
-    try {
-      final GoogleSignInAccount? googleUser = await GoogleSignIn(scopes: ['email']).signIn(); // Specify the desired scopes
-      final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-      final UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
-
-      // Check if the user is signed in
-      if (authResult.user != null) {
-        // Navigate to the next screen or perform any other action
-      }
-    } catch (e) {
-      // Handle errors (e.g., if the user cancels the sign-in)
-      print("Error signing in with Google: $e");
-    }
+    const String clientId =
+        '952786014191-4babja8nujkbdub2rgj9ej8fq10vboss.apps.googleusercontent.com';
   }
 
   @override
@@ -57,14 +38,37 @@ class SocalSignUp extends StatelessWidget {
             ),
             SocalIcon(
               iconSrc: "assets/icons/google-plus.svg",
-              press: _handleGoogleSignIn,
+              press: () {
+                _handleGoogleSignIn(context);
+              },
             ),
           ],
         ),
       ],
     );
   }
+
+  Future<void> _handleGoogleSignIn(BuildContext context) async {
+    try {
+      final GoogleSignInAccount? googleUser =
+      await GoogleSignIn(scopes: ['email']).signIn();
+      final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final UserCredential authResult =
+      await FirebaseAuth.instance.signInWithCredential(credential);
+
+      if (authResult.user != null) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+              (route) => false,
+        );
+      }
+    } catch (e) {
+      print("Error signing in with Google: $e");
+    }
+  }
 }
-
-
-
